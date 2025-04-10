c545c5f6272fdeed64e9dd8328e6998b8e702966
Magalaweng Clive Mmakola <clive.mmakola@bbd.co.za>
2025-04-10 08:28:23 +0200

    Progress

--- Cli/Services/AuthService.cs
+++ Cli/Services/AuthService.cs
@@ -2,6 +2,9 @@ using Cli.Commands;
 using Spectre.Console;
 using System.Diagnostics;
 using Cli.Models;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Cli.Services
 {
@@ -10,7 +13,8 @@ namespace Cli.Services
 
         public static int Login()
         {
-            string url = "https://example.com";
+            StartListener();
+            string url = "https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.profile&response_type=code&redirect_uri=http://localhost:8080/oauth2callback&client_id=438794123703-9aqbuhmv0asuhr074hqd5o2lf7c7rpap.apps.googleusercontent.com";
             AnsiConsole.MarkupLine($"[green]Open the following URL to login: {url}[/]");
 
             try
@@ -30,6 +34,26 @@ namespace Cli.Services
                 return 1; // Return non-zero to indicate an error
             }
             return 0; // Return zero to indicate success
+        }
+
+        public static HttpListener  StartListener() 
+        {
+            try
+            {
+                int port = 8080;
+                string prefix = $"http://localhost:{port}/"; 
+
+                HttpListener listener = new HttpListener();
+                listener.Prefixes.Add(prefix);
+
+                listener.Start();
+                return listener;
+            }
+            catch(Exception e)
+            {
+                new HttpListener();
+            }
+            return new HttpListener();
         }
     }
 }
