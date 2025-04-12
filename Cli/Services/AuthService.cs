c32ddbdd2759ea79fa5c450fdf1afc1d4ff8e661
Magalaweng Clive Mmakola <clive.mmakola@bbd.co.za>
2025-04-13 00:19:38 +0200

    Remove duplicate package and change number of attempts to request for token

--- Cli/Services/AuthService.cs
+++ Cli/Services/AuthService.cs
@@ -108,7 +108,7 @@ public class AuthService
  
     private async Task<Token?> PollForTokenAsync(string sessionId)
     {
-        int maxAttempts = 5;
+        int maxAttempts = 30;
         int attempts = 0;
  
         while (attempts < maxAttempts)
