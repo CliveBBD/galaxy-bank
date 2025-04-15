8479187352e138172cf1acd3f78ebc0459e32994
katlego-sekoele-bbd <katlego.sekoele@bbd.co.za>
2025-04-16 00:17:46 +0200

    Added a function to get the current user based on the auth header and added index on email column (#55)

--- Api/Repositories/UserRepository.cs
+++ Api/Repositories/UserRepository.cs
@@ -8,6 +8,7 @@ namespace Api.Repositories
     public interface IUserRepository
     {
         Task<User?> GetUserByIdAsync(int id);
+        Task<User?> GetUserByEmailAsync(string email);
     }
 
     public class UserRepository : IUserRepository
@@ -41,6 +42,37 @@ namespace Api.Repositories
                     return user;
                 },
                 new { Id = id },
+                splitOn: "RoleID"
+            );
+
+            return result.FirstOrDefault();
+        }
+
+        public async Task<User?> GetUserByEmailAsync(string email)
+        {
+            var query = $@"
+                SELECT 
+                    u.user_id AS {nameof(User.UserID)},
+                    u.google_id AS {nameof(User.GoogleID)},
+                    u.username AS {nameof(User.Username)},
+                    u.email AS {nameof(User.Email)},
+                    r.role_id AS RoleID,
+                    r.name AS Name
+                FROM users u
+                INNER JOIN roles r ON u.role_id = r.role_id
+                WHERE u.email = @Email
+            ";
+
+            using var connection = new NpgsqlConnection(Constants.ConnectionString);
+
+            var result = await connection.QueryAsync<User, Role, User>(
+                query,
+                (user, role) =>
+                {
+                    user.Role = role;
+                    return user;
+                },
+                new { Email = email },
                 splitOn: "RoleID"
             );
 
--- Api/Services/UserService.cs
+++ Api/Services/UserService.cs
@@ -1,11 +1,14 @@
 using Api.Models;
 using Api.Repositories;
+using Google.Apis.Auth;
 
 namespace Api.Services
 {
     public interface IUserService
     {
         Task<User> GetUserById(int id);
+        Task<User> GetUserByEmail(string email);
+        Task<User?> GetCurrentUser(HttpContext httpContext);
     }
     public class UserService : IUserService
     {
@@ -19,6 +22,25 @@ namespace Api.Services
             return await _userRepository.GetUserByIdAsync(id);
         }
 
+        public async Task<User?> GetUserByEmail(string email)
+        {
+            return await _userRepository.GetUserByEmailAsync(email);
+        }
+
+        public async Task<User?> GetCurrentUser(HttpContext httpContext)
+        {
+            var authHeader = httpContext.Request.Headers["Authorization"].ToString();
+            if (!string.IsNullOrEmpty(authHeader) && authHeader.StartsWith("Bearer "))
+            {
+                var jwt = authHeader.Substring("Bearer ".Length).Trim();
+                var payload = await GoogleJsonWebSignature.ValidateAsync(jwt);
+                return await this.GetUserByEmail(payload.Email);
+            }
+            else
+            {
+                return null;
+            }
+        }
 
     }
 }
