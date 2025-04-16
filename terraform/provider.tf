427c3cad83aa887863a30e810297ad5e77398a93
Vuyo Mama <65687571+KeloDraken@users.noreply.github.com>
2025-04-16 14:30:42 +0200

    Rewrite terraform setup (#77)
    * Rearrange workflow
    
    * Change region to af-south-1
    
    * Use roles
    
    * Downgrade aws cred action version
    
    * Add read/write permissions
    
    * Update state bucket name
    
    * Add echos
    
    * Add find
    
    * Remove login attempt
    
    * Login to ECR
    
    * Configure creds
    
    * Expose 8080
    
    * Switch regions
    
    * Create terraform setup
    
    ---------
    
    Co-authored-by: Jag kan HTML <vmama@ombank.co.za>

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,23 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-  backend "s3" {
-    bucket         = "galaxy-bank-tf-state"
-    key            = "terraform.tfstate"
-    region         = "af-south-1"
-    dynamodb_table = "galaxy-bank-state-lock-table"
-  }
-}
-
 provider "aws" {
-  region = "af-south-1"
-}
-
-provider "aws" {
-  alias  = "us_east_1"
-  region = "us-east-1"
+  region     = var.region
 }
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,27 +1,28 @@
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = var.subnet_cidrs
+  enable_dns_support   = true
+  enable_dns_hostnames = var.enable_dns_hostnames
 }
 
-resource "aws_subnet" "public_subnets" {
-  count                   = 2
+resource "aws_subnet" "public_subnet1" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
-  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
+  availability_zone       = "${var.region}a"
 }
 
-resource "aws_subnet" "private_subnets" {
-  count             = 2
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index + 2)
-  availability_zone = data.aws_availability_zones.available.names[count.index + 1]
+resource "aws_subnet" "public_subnet2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "${var.region}b"
 }
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 }
 
-resource "aws_route_table" "public_rt" {
+resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
   route {
@@ -30,32 +31,12 @@ resource "aws_route_table" "public_rt" {
   }
 }
 
-resource "aws_route_table_association" "public_rta" {
-  count          = 2
-  subnet_id      = aws_subnet.public_subnets[count.index].id
-  route_table_id = aws_route_table.public_rt.id
+resource "aws_route_table_association" "public_subnet1" {
+  subnet_id      = aws_subnet.public_subnet1.id
+  route_table_id = aws_route_table.public.id
 }
 
-resource "aws_eip" "nat_gateway_eip" {
-  vpc = true
-}
-
-resource "aws_nat_gateway" "nat_gateway" {
-  allocation_id = aws_eip.nat_gateway_eip.id
-  subnet_id     = aws_subnet.public_subnets[0].id
-}
-
-resource "aws_route_table" "private_rt" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat_gateway.id
-  }
-}
-
-resource "aws_route_table_association" "private_rta" {
-  count          = 2
-  subnet_id      = aws_subnet.private_subnets[count.index].id
-  route_table_id = aws_route_table.private_rt.id
+resource "aws_route_table_association" "public_subnet2" {
+  subnet_id      = aws_subnet.public_subnet2.id
+  route_table_id = aws_route_table.public.id
 }
