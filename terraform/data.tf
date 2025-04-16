a90ebf8373cd980fd3a21ec3b931d5de74801272
Vuyo Mama <65687571+KeloDraken@users.noreply.github.com>
2025-04-17 01:59:44 +0200

    Get secret value (#133)
    * Rearrange workflow
    
    * Change region to af-south-1
    
    * Use roles
    
    * Downgrade aws cred action version
    
    * Add read/write permissions
    
    * Update state bucket name
    
    * Add echos
    
    * Add find
    
    * Remove login attempt
    
    * Login to ECR
    
    * Configure creds
    
    * Expose 8080
    
    * Switch regions
    
    * Create terraform setup
    
    * Code formatting and clean up
    
    * Add variables terraform plan
    
    * Add required permissions to role
    
    * Remove terraform mutation
    
    * Add required environment variables
    
    * Code cleanup
    
    * Add Flyway migration action
    
    * Remove paths
    
    * Update Flyway version
    
    * Update Flyway pipeline
    
    * Add AWS authentication
    
    * Replace hard coded db values
    
    * List S3 bucket content
    
    * Run terraform init
    
    * Fix typo
    
    * Hard code db creds
    
    * Add runner permissions
    
    * Add auto approve flag
    
    * Remove extra arg
    
    * Fix permissions
    
    * Run actions sequentially
    
    * Run actions sequentially
    
    * Run actions sequentially
    
    * Run actions sequentially
    
    * s
    
    * ○
    
    * ○
    
    * Create release
    
    * Create release
    
    * Create release
    
    * Add tag
    
    * Add write perm to action
    
    * Fix typo
    
    * Remove font
    
    * Add Google secrets
    
    * Add Google secrets
    
    * Get secret value
    
    ---------
    
    Co-authored-by: Jag kan HTML <vmama@ombank.co.za>

---
+++ terraform/data.tf
@@ -0,0 +1,11 @@
+data "aws_secretsmanager_secret_version" "google_client_id" {
+  secret_id = aws_secretsmanager_secret.google_client_id.id
+}
+
+data "aws_secretsmanager_secret_version" "google_client_secret" {
+  secret_id = aws_secretsmanager_secret.google_client_secret.id
+}
+
+data "aws_secretsmanager_secret_version" "google_redirect_uri" {
+  secret_id = aws_secretsmanager_secret.google_client_secret.id
+}
