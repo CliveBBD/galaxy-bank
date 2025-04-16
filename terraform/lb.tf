5686ea9649d064bd6296aa6b5437de1655a684b0
Vuyo Mama <65687571+KeloDraken@users.noreply.github.com>
2025-04-16 10:30:17 +0200

    Rearrange workflow (#63)
    Co-authored-by: Jag kan HTML <vmama@ombank.co.za>

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -43,23 +43,3 @@ resource "aws_lb_target_group" "alb_target_group" {
   vpc_id      = aws_vpc.main.id
   target_type = "ip"
 }
-
-output "alb_dns_name" {
-  value       = aws_lb.alb.dns_name
-  description = "The DNS name of the Application Load Balancer"
-}
-
-output "ecr_repository_url" {
-  value       = aws_ecrpublic_repository.ecr_repository.repository_uri
-  description = "The URI of the public ECR repository"
-}
-
-output "ecs_cluster_name" {
-  value       = aws_ecs_cluster.ecs_cluster.name
-  description = "The name of the ECS cluster"
-}
-
-output "ecs_service_name" {
-  value       = aws_ecs_service.ecs_service.name
-  description = "The name of the ECS service"
-}
