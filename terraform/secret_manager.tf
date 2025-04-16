8e1bae9b4e103bd831b32168fc3fddae1381f3be
Vuyo Mama <65687571+KeloDraken@users.noreply.github.com>
2025-04-17 01:18:27 +0200

    Add Google secrets (#130)
    * Rearrange workflow
    
    * Change region to af-south-1
    
    * Use roles
    
    * Downgrade aws cred action version
    
    * Add read/write permissions
    
    * Update state bucket name
    
    * Add echos
    
    * Add find
    
    * Remove login attempt
    
    * Login to ECR
    
    * Configure creds
    
    * Expose 8080
    
    * Switch regions
    
    * Create terraform setup
    
    * Code formatting and clean up
    
    * Add variables terraform plan
    
    * Add required permissions to role
    
    * Remove terraform mutation
    
    * Add required environment variables
    
    * Code cleanup
    
    * Add Flyway migration action
    
    * Remove paths
    
    * Update Flyway version
    
    * Update Flyway pipeline
    
    * Add AWS authentication
    
    * Replace hard coded db values
    
    * List S3 bucket content
    
    * Run terraform init
    
    * Fix typo
    
    * Hard code db creds
    
    * Add runner permissions
    
    * Add auto approve flag
    
    * Remove extra arg
    
    * Fix permissions
    
    * Run actions sequentially
    
    * Run actions sequentially
    
    * Run actions sequentially
    
    * Run actions sequentially
    
    * s
    
    * ○
    
    * ○
    
    * Create release
    
    * Create release
    
    * Create release
    
    * Add tag
    
    * Add write perm to action
    
    * Fix typo
    
    * Remove font
    
    * Add Google secrets
    
    ---------
    
    Co-authored-by: Jag kan HTML <vmama@ombank.co.za>

--- terraform/secret_manager.tf
+++ terraform/secret_manager.tf
@@ -6,3 +6,19 @@ resource "aws_secretsmanager_secret_version" "db_connection_version" {
   secret_id     = aws_secretsmanager_secret.db_connection.id
   secret_string = "Host=${aws_db_instance.postgres.address};Port=5432;Database=${var.db_name};Username=${var.db_username};Password=${var.db_password}"
 }
+
+resource "aws_secretsmanager_secret" "google_client_id" {
+  name        = "${var.app_name}/google-client-id"
+  description = "Google OAuth Client ID"
+}
+
+resource "aws_secretsmanager_secret" "google_client_secret" {
+  name        = "${var.app_name}/google-client-secret"
+  description = "Google OAuth Client Secret"
+}
+
+resource "aws_secretsmanager_secret" "google_redirect_uri" {
+  name        = "${var.app_name}/google-client-redirect-uri"
+  description = "Google OAuth Redirect URI"
+}
+
