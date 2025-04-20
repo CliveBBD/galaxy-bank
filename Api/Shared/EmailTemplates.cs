7ec12ef54303549e9ca23a38a27195d86aaeeade
Mukhunyeledzi-Muthuphei <Mukhunyeledzi.Muthuphei@bbd.co.za>
2025-04-20 18:21:42 +0200

    editing email templates for transactions

--- Api/Shared/EmailTemplates.cs
+++ Api/Shared/EmailTemplates.cs
@@ -70,70 +70,6 @@ namespace Api.Shared
 
     }
 
-    public static class TransferEmailTemplate
-    {
-        public static string Subject = "Transfer Confirmation";
-
-        public static string Message(string senderName, string receiverName, string amount, string accountNumber)
-        {
-            var message = $@"
-                            <html>
-                            <head>
-                            <style>
-                                body {{
-                                font-family: Arial, sans-serif;
-                                background-color: #f4f4f4;
-                                padding: 20px;
-                                color: #333;
-                                }}
-                                .container {{
-                                max-width: 600px;
-                                margin: auto;
-                                background-color: #ffffff;
-                                padding: 30px;
-                                border-radius: 10px;
-                                box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
-                                }}
-                                .header {{
-                                text-align: center;
-                                color: #4A90E2;
-                                }}
-                                .content {{
-                                margin-top: 20px;
-                                line-height: 1.6;
-                                }}
-                                .footer {{
-                                margin-top: 30px;
-                                font-size: 0.9em;
-                                color: #777;
-                                text-align: center;
-                                }}
-                            </style>
-                            </head>
-                            <body>
-                            <div class='container'>
-                                <h2 class='header'>Transfer Confirmation</h2>
-                                <div class='content'>
-                                    <p>Dear {senderName},</p>
-                                    <p>Your transfer of <span style={"color:red;"}>{amount}</span> to {receiverName} has been successfully processed.</p>
-                                    <p><strong>Account Number:</strong> {accountNumber}</p>
-                                    <p>If you have any questions or need assistance, please contact our support team.</p>
-                                    <p style={"color: red;"}> If this was not you, please dispute the transfer via the CLI immediately.</p>
-                                    </div>
-                                    <div class='footer'>
-                                    &copy; {DateTime.Now.Year} Galaxy Bank. All rights reserved.
-                                    </div>
-                            </div>
-                            </body>
-                            </html>
-                            ";
-
-            return message;
-
-        }
-
-    }
-
     public static class DepositEmailTemplate
     {
         public static string Subject = "Deposit Confirmation";
@@ -180,7 +116,7 @@ namespace Api.Shared
                                 <div class='content'>
                                     <p>Dear {username},</p>
                                     <p>We are pleased to inform you that a deposit of <span style={"color:green;"}>Q {amount}</span> has been successfully credited to your account.</p>
-                                    <p><strong>Account Number:</strong> {accountNumber}</p>
+                                    <p><strong>Account Number:</strong> *******{accountNumber[^4..]}</p>
                                     <p>If you have any questions or need assistance, please contact our support team.</p>
                                 </div>
                                 <div class='footer'>
@@ -241,7 +177,7 @@ namespace Api.Shared
                                 <div class='content'>
                                     <p>Dear {username},</p>
                                     <p>We are writing to confirm that a withdrawal of <span style={"color:red;"}>-Q {amount}</span> has been successfully processed from your account.</p>
-                                    <p><strong>Account Number:</strong> {accountNumber}</p>
+                                    <p><strong>Account Number:</strong> *****{accountNumber[^4..]}</p>
                                     <p>If you have any questions or need assistance, please contact our support team.</p>
                                 </div>
                                 <div class='footer'>
@@ -302,8 +238,8 @@ namespace Api.Shared
                                 <div class='content'>
                                     <p>Dear {senderName},</p>
                                     <p>Your transfer of <span style={"color:red;"}>-Q {amount}</span> to {receiverName} has been successfully processed.</p>
-                                    <p><strong>From Account Number:</strong> *****{fromAccountNumber[^1..]}</p>
-                                    <p><strong>To Account Number:</strong> *****{toAccountNumber[^1..]}</p>
+                                    <p><strong>From Account Number:</strong> *****{fromAccountNumber[^4..]}</p>
+                                    <p><strong>To Account Number:</strong> *****{toAccountNumber[^4..]}</p>
                                     <p>If you have any questions or need assistance, please contact our support team.</p>
                                     <p style={"color: red;"}> If this was not you, please dispute the transfer via the CLI immediately.</p>
                                 </div>
@@ -365,8 +301,8 @@ namespace Api.Shared
                                 <div class='content'>
                                     <p>Dear {receiverName},</p>
                                     <p>You have received <span style={"color:green;"}>{amount}</span> from {senderName}.</p>
-                                    <p><strong>To Account Number:</strong> {toAccountNumber}</p>
-                                    <p><strong>From Account Number:</strong> {fromAccountNumber}</p>
+                                    <p><strong>To Account Number:</strong> ******{toAccountNumber[^4..]}</p>
+                                    <p><strong>From Account Number:</strong> ******{fromAccountNumber[^4..]}</p>
                                     <p>If you have any questions or need assistance, please contact our support team.</p>
                                 </div>
                                 <div class='footer'>
