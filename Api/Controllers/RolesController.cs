68e5d4debf8b7e7b619a10132f9336d8cafe7117
Mukhunyeledzi-Muthuphei <Mukhunyeledzi.Muthuphei@bbd.co.za>
2025-04-20 18:45:09 +0200

    removing user service from roles controller

--- Api/Controllers/RolesController.cs
+++ Api/Controllers/RolesController.cs
@@ -7,10 +7,9 @@ namespace Api.Controllers
 {
     [ApiController]
     [Route("roles")]
-    public class RolesController(IRoleService roleService, IUserService userService) : Controller
+    public class RolesController(IRoleService roleService) : Controller
     {
         private readonly IRoleService _roleService = roleService;
-        private readonly IUserService _userService = userService;
 
         [HttpGet("", Name = "GetRoles")]
         public async Task<IActionResult> GetRoles()
